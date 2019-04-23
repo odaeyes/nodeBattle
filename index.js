2545775c2ec556aa5a7cc115c0075725785410a2
odaeyes <blavier.jonathan@gmail.com>
2019-04-23 17:01:22 +0200

    superchat

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	io.emit('message','an user connected');
+	socket.on('disconnect',function(){
+		io.emit('message','an user disconnected');
+	});
+  socket.on('message', function(msg){
+    io.emit('message', msg);
+
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
