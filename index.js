b18f62edf99cf1bd25e0ff83cc122426de874a0f
marween <annemagali.saintgeorges@gmail.com>
2019-04-24 16:52:03 +0200

    chat ok

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket,pseudo){
@@ -11,17 +11,20 @@ io.on('connection', function(socket,pseudo){
 		socket.pseudo = pseudo;
 		io.emit('user', pseudo);
 	})
-	io.emit('message', pseudo+ ' connected');
-	socket.on('disconnect',function(pseudo){
-		io.emit('message',pseudo+ ' disconnected');
+	
+	socket.on('message', function(message){
+		io.emit('message', {pseudo: socket.pseudo, message: message});
 	});
 
-  socket.on('message', function(message){
-    io.emit('message', {pseudo: socket.pseudo, message: message});
 
-  });
+	socket.on('disconnect', function(pseudo){
+		io.emit('disconnect', socket.pseudo);
+	});
+
 });
 
+
+
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
