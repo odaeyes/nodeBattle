7018594a5e00a012d213520f5b13923688b7e327
odaeyes <blavier.jonathan@gmail.com>
2019-04-25 09:54:45 +0200

    conf port hero

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
@@ -25,6 +25,6 @@ io.on('connection', function(socket,pseudo){
 
 
 
-http.listen(3000, function(){
-	console.log('listening on *:3000');
+http.listen(port, function(){
+	console.log('listening on ${ port }');
 });
